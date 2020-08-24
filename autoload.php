9e42dce1d5bd539f0a5d4734e70a605861acc9bd
Alexandre Carretero <aleecarretero@gmail.com>
2020-08-24 18:57:06 -0300

    add comment

--- autoload.php
+++ autoload.php
@@ -3,7 +3,7 @@
 spl_autoload_register(function (string $nomeCompletoDaClasse) {
     $caminhoArquivo = str_replace('Alura\\Banco', 'src', $nomeCompletoDaClasse);
     $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
-    $caminhoArquivo .= '.php';
+    $caminhoArquivo .= '.php'; //Add extension
 
     if (file_exists($caminhoArquivo)) {
         require_once $caminhoArquivo;
